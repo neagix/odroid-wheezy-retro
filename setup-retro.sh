35d7c5e5a0cf11f28c12c87511e85a91a4e4338e
neagix <neagix@gmail.com>
2013-05-08 19:29:02 +0200

    Initial retro setup script

---
+++ setup-retro.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+## @author: neagix
+## update scripts, pull all necessary sources and build them
+#
+
+## stop on any error
+set -e
+
+git fetch
+if [ $(git log HEAD..origin | grep sh) ]; then
+	echo "Some scripts updated, please run again"
+	git pull -v &
+	exit
+fi
+
+function build_retroarch() {
+	DEFOPTS="--enable-alsa --disable-oss --disable-jack --disable-pulse --enable-xvideo"
+
+	cd retroarch
+	./configure $DEFOPTS --enable-x11 --enable-opengl --enable-gles --disable-kms --disable-vg && \
+	make -j5
+}
