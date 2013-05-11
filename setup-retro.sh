4347360b29ce4431c4fcdb2e795e1dca043a0c85
neagix <neagix@gmail.com>
2013-05-11 09:50:43 +0200

    Modified scripts to compile retroarch & test it

--- setup-retro.sh
+++ setup-retro.sh
@@ -19,9 +19,26 @@ function build_retroarch() {
 	cd retroarch
 	./configure $DEFOPTS --enable-x11 --enable-opengl --enable-gles --disable-kms --disable-vg && \
 	make -j5
+	strip retroarch tools/retroarch-joyconfig
+	cp retroarch tools/retroarch-joyconfig ~/bin
+}
+
+function build_snes9x() {
+	cd libretro-snes9x
+	CFLAGS="$CFLAGS -mfpu=neon -mfloat-abi=hard" CXXFLAGS="$CXXFLAGS -mfpu=neon -mfloat-abi=hard" ASFLAGS="$ASFLAGS -mfpu=neon -mfloat-abi=hard" \
+	make -j5 -f Makefile.libretro
+	strip snes9x_next_libretro.so
+	cp snes9x_next_libretro.so ~/.retroarch/cores/snes9x.so
 }
 
 if [ ! -d retroarch ]; then
 	git clone https://github.com/neagix/RetroArch.git retroarch --depth=0
 fi
 build_retroarch
+
+if [ ! -d libretro-snes9x ]; then
+	git clone https://github.com/libretro/snes9x-next.git libretro-snes9x --depth=0
+fi
+build_snes9x
+
+## see also https://github.com/libretro/libretro-super
--- tests/retroarch-test.sh
+++ tests/retroarch-test.sh
@@ -5,4 +5,4 @@
 
 echo "You should now see a demo running through snes9x emulation"
 echo "- - - - - -"
-DISPLAY=:0 retroarch "$HOME/roms/SNES9X Demo V1.16 (PD).smc"
+DISPLAY=:0 retroarch /home/emuser/roms/FrogNes.smc
