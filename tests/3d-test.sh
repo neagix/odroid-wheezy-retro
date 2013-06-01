c380b26afa0fd6e92798db4790320b08e1c9fd2a
neagix <neagix@gmail.com>
2013-06-01 12:14:45 +0200

    Added ARM optimizations for ODROIDs Added README for retro release Removing old useless scripts

--- tests/3d-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-## @author neagix
-## run this as 'emuser'
-#
-
-echo "You should now see GLES2 3D gears animation and FPS count should be around 432"
-echo "- - - - -"
-DISPLAY=:0 /home/emuser/bin/es2gears
--- tests/retroarch-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-## @author neagix
-## run this as 'emuser'
-#
-
-echo "You should now see a demo running through snes9x emulation"
-echo "- - - - - -"
-DISPLAY=:0 retroarch /home/emuser/roms/FrogNes.smc
